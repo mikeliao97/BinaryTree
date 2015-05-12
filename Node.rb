85fdb23945a7e7770345cfe86efde22476cfd254
Qin Liao <Mikeliao97@gmail.com>
2015-05-11 20:46:41 -0700

    finished binary tree

--- Node.rb
+++ Node.rb
@@ -1,14 +1,23 @@
 class Node
 	attr_accessor :left, :right, :value, :parent
 	
-	def initialize(value, left = nil, right = nil, parent = nil)
+	def initialize(value,  parent = nil,  left = nil, right = nil)
 		@value = value
 		@left = left	
 		@right = right	
 		@parent = parent	
 	end	
 	
-	
+	def to_s 
+		str = ""
+		str += @value.nil? ? "\t@value: Nil\n" : "\t@value: #{@value}\n"
+		str += @parent.nil? ? "\t@parent: Nil\n" : "\t@parent: #{@parent.value}\n"
+		str += @left.nil? ? "\t@left: Nil\n" : "\t@left: #{@left.value}\n"
+		str += @right.nil? ? "\t@right: Nil\n" : "\t@right: #{@right.value}\n"
+		return 	str
+	end
+		
+
 
 end
 
--- binary_tree.rb
+++ binary_tree.rb
@@ -1,30 +1,71 @@
+
+
 require_relative 'Node.rb'
 
 
 class BinaryTree
 	attr_accessor :tree, :array_of_nodes
-	
+		
 	def build_tree(array)
+		@array_of_nodes = []
+		@tree = Node.new(array[0]) #initialize the first element of the array as a node
+
+		@array_of_nodes << array.shift
+
 		array.each do |value|
 			insert(value, @tree)	
 		end
 	end
+	
+	def insert(value, node = @tree)
+		unless value == node.value
+	
+				if value < node.value
+					if node.left.nil? 
+						node.left = Node.new(value, node, nil, nil) 
+						@array_of_nodes << node.left
+					else 
+						insert(value, node.left)
+					end
 
-	def insert(value, node)
-		if(node.nil?)
-			node = Node.new(value)
-		elsif(value > node.value) 
-			insert(value, node.right)	
-		else if(value < node.value)
-			insert(value, node.left)
+				else
+					if node.right.nil?
+						node.right = Node.new(value, node, nil, nil) 
+						@array_of_nodes << node.right
+					else
+						insert(value, node.right)
+					end
+				end
+
 		end
-	end 
+	end
+
+	def print_tree
+		
+		
+
+	end
+end
+
+
+
+driver = BinaryTree.new
+ 
+driver.build_tree([100, 0, 1000, 3232, 32, 2, 1])
+
+puts driver.tree
+
+driver.array_of_nodes.each do |node|
+	puts node
+	puts ""
 
 end
 
-arr = [1,2,3,4,5,6,7]
 
-tree = BinaryTree.new
 
-tree.build_tree(arr)
+
+
+
+
+
 
